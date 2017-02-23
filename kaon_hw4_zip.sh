9121f0b687bbef792b16dd2031595bd86a768987
Jeremy J. Johnson <jeremyjohnson7@mail.weber.edu>
2017-02-23 12:45:25 -0700

    Added zip

---
+++ kaon_hw4_zip.sh
@@ -0,0 +1,25 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: kaon_hw4_compress.sh
+# 
+#         USAGE: ./kaon_hw4_compress.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Jeremy Johnson (), jeremyjohnson7@mail.weber.edu
+#  ORGANIZATION: WSU
+#       CREATED: 02/23/2017 11:33
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+zip MOCK_DATA_FILTER_YYYY_MM_DD_HH:MM.zip *
+
+exit 0
+
