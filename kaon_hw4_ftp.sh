c03186f74f157a3bf9cb2ed5952f40c51e8662db
Jeremy J. Johnson <jeremyjohnson7@mail.weber.edu>
2017-02-23 12:47:14 -0700

    Added ftp script

---
+++ kaon_hw4_ftp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: kaon_hw4_ftp.sh
+# 
+#         USAGE: ./kaon_hw4_ftp.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Jeremy Johnson (), jeremyjohnson7@mail.weber.edu
+#  ORGANIZATION: WSU
+#       CREATED: 02/23/2017 12:46
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
+
+exit 0
+
