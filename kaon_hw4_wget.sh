1121adc68938b76cbf37cbcfe5057f726ab4199c
NasserDos <nasserbinshabeeb@mail.weber.edu>
2017-02-22 22:03:14 -0700

    wget scrpit completed

--- kaon_hw4_wget.sh
+++ kaon_hw4_wget.sh
@@ -19,6 +19,18 @@
 
 #set -o nounset                              # Treat unset variables as an error
 
+help(){
+ echo "Usage   ./kaon_hw4_wget.sh <input> "
+
+}
+
+#-q will silence the messages associated with wget 
+# --spider will check if the file exists (200 OK if exists)
+# && will execute if the test before it is true 
+# || will execute if the test before it is fails
+
+# if file exists DOWNLOAD , PRINT CONFIRMATION , otherwise print it failed 
+
+wget -q --spider http://icarus.cs.weber.edu/~hvalle/cs3030/MOCK_DATA_$1.tar.gz && wget -q http://icarus.cs.weber.edu/~hvalle/cs3030/MOCK_DATA_$1.tar.gz && echo File Downloaded successfully || echo File does not exits
 
 exit 0
-
