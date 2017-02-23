76fd253d1d218253c6c6b7f75a4825c4155d8c03
NasserDos <nasserbinshabeeb@mail.weber.edu>
2017-02-22 21:29:23 -0700

    wget script

---
+++ kaon_hw4_wget.sh
@@ -0,0 +1,24 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: kaon_hw4_wget.sh
+# 
+#         USAGE: ./kaon_hw4_wget.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Nasser Binshabeeb 
+#  ORGANIZATION: 
+#       CREATED: 02/22/2017 21:27
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
+exit 0
+
